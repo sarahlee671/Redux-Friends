22389608bb0eaf45b81211efc9a678d4c34f9d0f
Sarahlee671 <sarah.lee1987@hotmail.com>
2019-05-16 12:29:54 -0700

    Added routes and links to App.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,29 +1,28 @@
 import React from 'react';
-import {Route, NavLink} from 'react-router-dom';
+import {Route, Link} from 'react-router-dom';
 import './App.css';
 import LoginForm from './components/LoginForm';
-import FriendsList from '../src/components/FriendsList'
+import FriendsList from '../src/components/FriendsList';
+import PrivateRoute from './PrivateRoute';
 
-class App extends React.Component {
+function App() {
 
-  render() {
-    return (
-      <div className="App">
+  return (
+    <div className="App">
 
-        <h1 className="title">My Title</h1>
-        <nav className="nav-links">
-          <NavLink exact to="/login">Login</NavLink>
+      <h1 className="title">My Title</h1>
+      <nav className="nav-links">
+        <Link to="/login">Login</Link>
+        <Link to="/protected">Protected Page</Link>
+      </nav>
+    
 
-        </nav>
-        {/* <LoginForm /> */}
-        <FriendsList />
-        
-        <Route path="/login" component={LoginForm} />
-        {/* <PrivateRoute exact path ="/protected" component={FriendsListView} /> */}
-         
-      </div>
-    );
-  }
+      <Route path="/login" component={LoginForm} />
+      <PrivateRoute exact path ="/protected" component={FriendsList} />
+
+    </div>
+  );
+
 }
 
 export default App;
