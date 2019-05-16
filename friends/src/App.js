e76de60eb0204cccc09008ca0da8bfe8f7cbbf63
Sarahlee671 <sarah.lee1987@hotmail.com>
2019-05-15 21:08:20 -0700

    Created reducers for fetching friends and login

--- friends/src/App.js
+++ friends/src/App.js
@@ -3,15 +3,23 @@ import {Route} from 'react-router-dom';
 import './App.css';
 import LoginForm from './components/LoginForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div className="App">
+
         <h1 className="title">My Title</h1>
-      </header>
-      <Route path="/login" render={props => <LoginForm />} />
-    </div>
-  );
+        <nav className="nav-links">
+          <NavLink exact to="/login">Login</NavLink>
+
+        </nav>
+        <Route path="/login" render={props => <LoginForm />} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- friends/src/reducers/index.js
+++ friends/src/reducers/index.js
@@ -1,3 +1,45 @@
 const initialState = {
-    friends = {}
+    friends: [],
+    fetchingFriends: false,
+    error: '',
+    token: ''
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case FETCH_FRIENDS_START:
+            return {
+                ...state,
+                fetchingFriends: true,
+            };
+        case FETCH_FRIENDS_SUCCESS:
+            return {
+                ...state,
+                fetchingFriends: false,
+                friends: [...state.friends, ...action.payload]
+            };
+        case FETCH_FRIENDS_ERROR:
+            return {
+                ...state,
+                fetchingFriends: false,
+                error: action.payload
+            };
+        case LOGIN_START:
+            return {
+                ...state,
+            };
+        case LOGIN_SUCCESS:
+            return {
+                ...state,
+                error: '',
+                token: action.payload
+            };
+        case LOGIN_ERROR:
+            return {
+                ...state,
+                error: action.payload
+            }
+        default:
+            return state;
+    }
 }
